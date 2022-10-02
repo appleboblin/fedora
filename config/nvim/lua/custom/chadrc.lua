d1bb4696863f0b7746edf9b7de7490187eb8bc1c
appleboblin <appleboblin2430@gmail.com>
2022-10-02 01:26:54 -0700

    nvim mapping

---
+++ config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+M.ui = {
+  theme = "nord",
+}
+
+M.mappings = require "custom.mappings"
+
+return M
---
+++ config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+M.colemak = {
+  n = {
+   ["n"] = { "j", "Cursor down" },
+   ["e"] = { "k", "Cursor up" },
+   ["i"] = { "l", "Cursor right" },
+   ["u"] = { "i", "Insert mode" },
+   ["U"] = { "I", "" },
+   ["l"] = { "u", "" },
+  }
+
+}
+
+return M
+
